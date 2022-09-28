7c0bd79776c546e0505e235b8ec599b8b1128239
Xoke <xokesoru@gmail.com>
2022-09-28 12:24:21 -0700

    Update get-powershell.bat

--- get-powershell.bat
+++ get-powershell.bat
@@ -4,6 +4,3 @@ COPY C:\Windows\System32\WindowsPowerShell\v1.0 PowerShell
 CD PowerShell
 ECHO >>powershell.exe
 ECHO >>powershell_ise.exe
-
-rem Copies the powershell folder and adds a byte to the end to bypass some checks
-rem add byte taken from https://blog.certcube.com/powershell-restrictions-bypasses-archives-learnings/
